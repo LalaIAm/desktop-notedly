e97c6a4d27a7622e19badd987a99910fbb71970b
Lauren Thorud <lthorud12@gmail.com>
2021-10-22 11:55:21 -0500

    ready

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  LOCAL_WEB_URL: 'http://localhost:1234',
+  PRODUCTION_WEB_URL: 'https://determined-aryabhata-4d25f6.netlify.app/',
+  PRODUCTION_API_URL: 'https://notedly-api-2.herokuapp.com/graphql',
+};
+
+module.exports = config;
